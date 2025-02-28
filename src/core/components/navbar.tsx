32f3c26d91cffacf5cab0b35be84324a6a2d78ac
Gerardo Ruiz Mendez <ingeniero.subcontexto@gmail.com>
2025-02-28 16:45:44 -0600

    Adding marco juridico

--- src/core/components/navbar.tsx
+++ src/core/components/navbar.tsx
@@ -7,7 +7,7 @@ interface NavBarProps {
 
 function NavBar({ imageSrcPath, navItems }: NavBarProps) {
   const location = useLocation(); // Obtiene la ruta actual
-  const links = ["/", "/acerca", "/estadistica", "/mapa"]; // Rutas correspondientes
+  const links = ["/", "/acerca", "/estadistica", "/mapa", "/marco-juridico"]; // Rutas correspondientes
 
   return (
     <nav className="bg-white shadow-md w-full">
@@ -32,11 +32,10 @@ function NavBar({ imageSrcPath, navItems }: NavBarProps) {
               <li key={item}>
                 <Link
                   to={links[index] || "/"} // Evita posibles errores de índice
-                  className={`px-3 py-2 block transition duration-300 ${
-                    isActive
-                      ? "font-bold text-blue-700 border-b-2 border-blue-700"
-                      : "text-gray-700 hover:text-blue-600"
-                  }`}
+                  className={`px-3 py-2 block transition duration-300 ${isActive
+                    ? "font-bold text-blue-700 border-b-2 border-blue-700"
+                    : "text-gray-700 hover:text-blue-600"
+                    }`}
                 >
                   {item}
                 </Link>
